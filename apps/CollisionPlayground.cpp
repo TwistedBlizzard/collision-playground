581913232c849e59370fd5bd3ab0387899ca8f21
Melanie Halsey <6956106+TwistedBlizzard@users.noreply.github.com>
2023-07-31 14:57:40 +0100

    Randomised spawn locations

--- apps/CollisionPlayground.cpp
+++ apps/CollisionPlayground.cpp
@@ -1,19 +1,31 @@
 #include "Engine/Engine.hpp"
 
-#define FRAMES 10000
+#include <random>
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
 
 int main()
 {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution distX(0, 200 * 1024);
+    uniform_int_distribution distY(0, 200 * 768);
+    uniform_int_distribution distR(5, 40);
+
     Engine* engine = new Engine();
 
     engine->Init();
 
-    engine->CreateGameObject(Vector2f(100.0f, 10.0f));
-    GameObjectPtr gameObject = engine->CreateGameObject(Vector2f(10.0f, 100.0f));
-    ColliderPtr collider = gameObject->GetCollider();
-    collider->SetRadius(20.0f);
-    engine->CreateGameObject(Vector2f(400.0f, 20.0f));
-    // engine->CreateGameObject(Vector2f(512.0f, 384.0f));
+    for (int i = 0; i < 300; ++i)
+    {
+        float x = 0.01f * distX(gen);
+        float y = 0.01f * distY(gen);
+        int r = distR(gen);
+
+        GameObjectPtr go = engine->CreateGameObject(Vector2f(x, y));
+        // go->GetCollider()->SetRadius(r);
+    }
 
     engine->Start();
 
